8133d861ac2e8bffb4c258d1b553a1179656f508
engalar <engalar@gmail.com>
2022-04-15 05:14:54 +0000

    Initial app upload.

---
+++ theme/styles/native/app/custom.js
@@ -0,0 +1,71 @@
+
+export const messageImage = {
+    container: {
+        borderRadius: 10,
+        borderWidth: 5,
+        borderColor: '#4799c9'
+    }
+}
+export const messageSent = {
+    container: {
+        
+        borderRadius: 10,
+        borderWidth: 2,
+        backgroundColor:'#99d9ff',
+        borderColor: '#4799c9',
+        shadowColor: '#4799c9',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 3,  
+        elevation: 6
+    }
+}
+
+export const messageRecieved = {
+    container: {
+        
+        borderRadius: 10,
+        borderWidth: 2,
+        backgroundColor:'#b6faec',
+        borderColor: '#3fb59c',
+        shadowColor: '#3fb59c',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 3,  
+        elevation: 6
+    }
+}
+
+export const profilePicture = {
+    container: {
+        borderRadius: 50,
+        borderWidth: 3,
+        borderColor: '#4799c9',
+        shadowColor: '#4799c9',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,  
+        elevation: 5
+    }
+}
+
+export const loginLogo = {
+    container: {
+        alignSelf: 'center',
+        flex: 1,
+        aspectRatio: 1.1, 
+        resizeMode: 'contain',
+        shadowColor: '#4799c9',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,  
+        elevation: 5
+    }
+}
+
+export const reverseList = {
+    container: {
+       display:'flex',
+       flexDirection:'column-reverse'
+    }
+}
